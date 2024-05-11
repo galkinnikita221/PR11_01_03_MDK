b083456caac5cfd1fd50b8732f4048e5538043f6
Nikita <galkinnikita228@gmail.com>
2024-05-11 15:30:59 +0500

    Реализована практика на оценку 5. Галкин

--- app/src/main/java/com/example/banks_galkin/MainActivity.java
+++ app/src/main/java/com/example/banks_galkin/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.banks_galkin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,186 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void Wallet1(View view) {
+        Toast toast = Toast.makeText(this, "Это кошелёк!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Wallet2(View view) {
+        Toast toast = Toast.makeText(this, "Скрыть данные!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void Wallet3(View view) {
+        Toast toast = Toast.makeText(this, "Ещё!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Wallet4(View view) {
+        Toast toast = Toast.makeText(this, "Это защита!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Wallet5(View view) {
+        Toast toast = Toast.makeText(this, "Это безопасность!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Wallet6(View view) {
+        Toast toast = Toast.makeText(this, "Ваша зарплатная карта!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Wallet7(View view) {
+        Toast toast = Toast.makeText(this, "Ваша резервная карта!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Wallet8(View view) {
+        Toast toast = Toast.makeText(this, "Ваша дополнительная карта!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Exit(View view) {
+        Toast toast = Toast.makeText(this, "Скрыть уведомление!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void All(View view) {
+        Toast toast = Toast.makeText(this, "Выбрать все данные!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Transfer(View view) {
+        Toast toast = Toast.makeText(this, "Новый перевод!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Transfer2(View view) {
+        Toast toast = Toast.makeText(this, "Перевод человеку!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Expenses(View view) {
+        Toast toast = Toast.makeText(this, "Все расходы!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Expenses2(View view) {
+        Toast toast = Toast.makeText(this, "Расходы транспорт!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Expenses3(View view) {
+        Toast toast = Toast.makeText(this, "Расходы рестораны и кафе!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Expenses4(View view) {
+        Toast toast = Toast.makeText(this, "Расходы на цветы!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Add(View view) {
+        Toast toast = Toast.makeText(this, "Добавить",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Add2(View view) {
+        Toast toast = Toast.makeText(this, "Оформить",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void AllServices(View view) {
+        Toast toast = Toast.makeText(this, "Все сервисы",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Sales(View view) {
+        Toast toast = Toast.makeText(this, "Скидки и предложения",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Adverting(View view) {
+        Toast toast = Toast.makeText(this, "Акция1",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Adverting2(View view) {
+        Toast toast = Toast.makeText(this, "Акция2",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Adverting3(View view) {
+        Toast toast = Toast.makeText(this, "Акция3",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Adverting4(View view) {
+        Toast toast = Toast.makeText(this, "Акция4",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Buy(View view) {
+        Toast toast = Toast.makeText(this, "Купить",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Sale(View view) {
+        Toast toast = Toast.makeText(this, "Продать",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Valute1(View view) {
+        Toast toast = Toast.makeText(this, "Юань",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Valute2(View view) {
+        Toast toast = Toast.makeText(this, "Доллар",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Valute3(View view) {
+        Toast toast = Toast.makeText(this, "Дирхам",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Menu(View view) {
+        Toast toast = Toast.makeText(this, "Домой",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Menu2(View view) {
+        Toast toast = Toast.makeText(this, "Накопления",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Menu3(View view) {
+        Toast toast = Toast.makeText(this, "Ассистент",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Menu4(View view) {
+        Toast toast = Toast.makeText(this, "Платежи",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Menu5(View view) {
+        Toast toast = Toast.makeText(this, "История",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Profile(View view) {
+        Toast toast = Toast.makeText(this, "Ваш профиль!",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void Basket(View view) {
+        Toast toast = Toast.makeText(this, "Ваш профиль!",Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
